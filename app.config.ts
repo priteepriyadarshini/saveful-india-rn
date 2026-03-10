b9d8ad039ee9c6f3c2e3d4c442a0bb3c189fa1f1
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2026-03-10 23:17:48 +0530

    fix

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'Saveful',
   slug: 'saveful-app',
   scheme: 'saveful',
-  version: '2.0.4',
+  version: '2.0.5',
   orientation: 'portrait',
 
   icon: './assets/icon.png',
